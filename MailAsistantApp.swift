b9e9f520e30a1bfcadd2e1b8c31363cd3403df1a
evgenii.ledin <evgenii.ledin@sap.com>
2025-12-27 18:03:10 +0100

    init codebase

---
+++ MailAsistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailAsistantApp.swift
+//  MailAsistant
+//
+//  Created by Ledin, Evgenii on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MailAsistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
